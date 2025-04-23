9dd23fcddfabad65e285cbabcb9c05319d0cdc70
vasylm1 <121822689+vasylm1@users.noreply.github.com>
2025-04-23 18:13:57 +0200

    Update config.php

--- user/config.php
+++ user/config.php
@@ -21,10 +21,10 @@ define( 'YOURLS_COOKIEKEY', 'fpsiaxh863g7hdy6oaqkgkbl6rmdu4sa
 ' );
 
 // ** Database settings **
-define( 'YOURLS_DB_USER', 'root' );
-define( 'YOURLS_DB_PASS', '' );
-define( 'YOURLS_DB_NAME', 'yourls' );
-define( 'YOURLS_DB_HOST', 'localhost' );
+define( 'YOURLS_DB_USER', 'shortly_i1sw_user' );
+define( 'YOURLS_DB_PASS', 'yFKiZMIjnlq0Rhl5MvBh0KuIJyLO82j2' );
+define( 'YOURLS_DB_NAME', 'shortly_i1sw' );
+define( 'YOURLS_DB_HOST', 'dpg-d04h23uuk2gs73bgtqd0-a' );
 define( 'YOURLS_DB_PREFIX', 'yourls_' );
 
 // Debug mode
