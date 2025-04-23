c11b1cc7f1a32c4724f466a6436fc2c98d6f565d
vasylm1 <121822689+vasylm1@users.noreply.github.com>
2025-04-23 20:22:10 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,25 @@
+import shortuuid
+import qrcode
+from io import BytesIO
+import validators
+
+def generate_short_code():
+    return shortuuid.uuid()[:8]
+
+def generate_qr_code(url):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill_color="black", back_color="white")
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    return buffered.getvalue()
+
+def is_valid_url(url):
+    return validators.url(url)
